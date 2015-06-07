d4643c4b3d8b7bf0607ebba1c0ea1195e5424913
Zhanelya Subebayeva <subebo91@inbox.ru>
2015-06-07 09:47:28 +0100

    Altered layout

--- source/contact.php
+++ source/contact.php
@@ -1,8 +1,8 @@
 <?php require_once 'header.php';?>
     <div class="container fluid">
         <h1>Contact</h1>
-        <p>Zhanelya Subebayeva </p>
-        <p>MSc Computer Science, QMUL</p>
-        <p>Telephone: +44 (0)754394523</p>
+        <h4>Zhanelya Subebayeva </h4>
+        <h4>MSc Computer Science, QMUL</h4>
+        <h4>Telephone: +44 (0)754394523</h4>
     </div>
 <?php require_once 'footer.php';?>
--- source/header.php
+++ source/header.php
@@ -5,13 +5,13 @@
         <meta name="author" content="Zhanelya Subebayeva">
         <meta name="description" content="Online tool for exploration of data sonification techniques">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-        <link type="text/css" rel="stylesheet" href="css/main.css">
+        <!--<link type="text/css" rel="stylesheet" href="css/main.css">-->
         <title></title>
     </head>
     <body>
         <nav>
             <ul>
-                <li class="nav-item"><a href="/">Home</a></li>
+                <li class="nav-item"><a href="index.php">Home</a></li>
                 <li class="nav-item"><a href="sonification.php">Sonification</a></li>
                 <li class="nav-item"><a href="contact.php">Contact</a></li>
             </ul>
