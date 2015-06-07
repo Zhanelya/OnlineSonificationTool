891cfbee8db31c8d07c2b6fee02c53491b6d815f
Zhanelya Subebayeva <subebo91@inbox.ru>
2015-06-07 12:42:07 +0100

    Altered menu

--- source/header.php
+++ source/header.php
@@ -5,19 +5,19 @@
         <meta name="author" content="Zhanelya Subebayeva">
         <meta name="description" content="Online tool for exploration of data sonification techniques">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-        <!--<link type="text/css" rel="stylesheet" href="css/main.css">-->
+        <link type="text/css" rel="stylesheet" href="css/main.css">
         <title></title>
     </head>
     <body>
-        <nav>
-            <ul>
-                <li class="nav-item"><a href="index.php">Home</a></li>
-                <li class="nav-item"><a href="sonification.php">Sonification</a></li>
-                <li class="nav-item"><a href="contact.php">Contact</a></li>
-            </ul>
+        <nav class="navbar navbar-default navbar-fixed-top ">
+                <ul class="nav text-center">
+                    <li class="col-md-4"> <a href="index.php">Home</a></li>
+                    <li class="col-md-4"> <a href="sonification.php">Sonification</a></li>
+                    <li class="col-md-4"> <a href="contact.php">Contact</a></li>
+                </ul>
         </nav>    
-
-        <input type="checkbox" id="nav-trigger" class="nav-trigger" />
-        <label for="nav-trigger"></label>
-
-        <div class="wrapper">
+        <div class="wrapper container-fluid">
+            
+            
+            
+            
