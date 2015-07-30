74f984c5b10665e18cee2e55d9f01109232bcc89
Zhanelya Subebayeva <subebo91@inbox.ru>
2015-07-30 11:52:10 +0100

    Added footer

--- source/footer.php
+++ source/footer.php
@@ -1,3 +1,9 @@
         </div>
+        <footer>
+            <div class="row-fluid">
+            <div class="text-center">Copyright &copy; 2015</div>
+            <div  class="text-center">Web design by Zhanelya Subebayeva</div>
+            </div>
+        </footer>
     </body>
 </html>
